5f896be7c2130e08a688c1ce3c2d18518c88d1ce
AlejandroBel27 <ruben.saldana216100@potros.itson.edu.mx>
2023-09-17 01:24:28 -0700

    Creacion de las clases

---
+++ src/main/java/persistencia/ProductoDAO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia;
+
+/**
+ *
+ * @author 216100
+ */
+public class ProductoDAO extends Conexion {
+
+    public ProductoDAO() {
+    }
+    
+    
+
+}
