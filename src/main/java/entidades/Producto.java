b0f45c1a3d58a67df1074db4df9df58e8418c664
AlejandroBel27 <ruben.saldana216100@potros.itson.edu.mx>
2023-09-17 01:46:18 -0700

    Se creo la entidad de producto y la persistencia de productoDAO

--- src/main/java/entidades/Producto.java
+++ src/main/java/entidades/Producto.java
@@ -12,5 +12,49 @@ package entidades;
 public class Producto {
     
     private String nombre; 
+    private String descripcion;
+    private String precio;
+    //private String idCategoria  //consultar con equipo para saber si lo pondremos o no.
+
+    public Producto() {
+    }
+    /**
+     * meto que ayuda a registrar un producto.
+     * @param nombre Nombre del producto. 
+     * @param descripcion Descripción del producto.
+     * @param precio Precio del producto.
+     */
+    public Producto(String nombre, String descripcion,String precio ){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+    
+    
+    
     
 }
--- src/main/java/persistencia/ProductoDAO.java
+++ src/main/java/persistencia/ProductoDAO.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package persistencia;
+import java.sql.PreparedStatement;
+import entidades.Producto;
 
 /**
  *
@@ -14,6 +11,33 @@ public class ProductoDAO extends Conexion {
     public ProductoDAO() {
     }
     
+  public boolean registrar (Producto producto){
+      PreparedStatement pst = null;
+      
+      try{
+          String consulta = "INSERT INTO  productos(nombre, direccion, precio) VALUES(?, ?, ?)";
+          pst = getConexion().prepareStatement(consulta);
+          pst.setString(1, producto.getNombre());
+          pst.setString(2, producto.getDescripcion());
+          pst.setString(3, producto.getPrecio());
+          
+          if(pst.executeUpdate() == 1){
+              System.out.println("Producto creado.");
+              return true;
+          }
+      }catch(Exception e){
+          System.out.println("Error en "+e);
+      }finally{
+          try{
+              if(getConexion() != null)getConexion().close();
+              if(pst != null)pst.close();
+          }catch(Exception e){
+              System.out.println("Error en "+e);
+          }
+      }
+      System.out.println("Producto no registrado");
+      return false;
+  }
     
 
 }
